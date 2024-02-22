f8dbc23f7b1c1fb062b3bd381ff10486e704df4d
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2024-02-22 22:35:14 +0530

    5th dump

--- Scripts.js
+++ Scripts.js
@@ -1,8 +1,14 @@
 
 
-document.addEventListener('click',)
-const change = function(){
+const colors = ["red", "green", "yellow"];
 
-    document.getElementsByClassName("container")[0].style.backgroundColor = "green"
+let index = 0;
+const block = document.getElementsByClassName("container")[0]
+document.addEventListener('click', ()=>{
 
-}
+block.style.backgroundColor = colors[index];
+
+index = (index+1)% colors.length
+
+
+})
